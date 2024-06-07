acde2959c025e260ced21dc8f4d34314dc374d11
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2024-06-07 16:24:54 +0300

    restoringTet2

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,23 +1,37 @@
-import svg from "../../img/icons.svg";
+import { useState } from "react";
+import EditBoardModal from "../EditBoardModal/EditBoardModal";
 
-export default function Board({ title, icon, bg }) {
+import svg from "../../img/icons.svg";
+import css from "./Board.module.css";
+
+export default function Board({ title, icon }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <li>
-      <h3>{title}</h3>
       <div>
         <svg width="24px" height="24px">
           <use href={`${svg}#${icon}`} />
         </svg>
       </div>
       <div>
-        <img
-          srcSet={`${bg.mob} 375w, ${bg.tab} 768w, ${bg.desc} 1180w`}
-          sizes="(max-width: 767px) 375px, (min-width: 768px) 768px, (min-width: 1440px) 1180px"
-          src={bg.mini}
-          alt={bg.id}
-          width="28px"
-          height="28px"
-        />
+      <h3>{title}</h3>
+      <button type="button" onClick={openModal}>
+        <svg className={css.icon} width="20px" height="20px">
+          <use href={svg + "#icon-pencil"}></use>
+        </svg>
+      </button>
+      {isModalOpen && (
+        <EditBoardModal isOpen={isModalOpen} onClose={closeModal} />
+      )}
       </div>
     </li>
   );
--- src/components/BoardList/BoardList.jsx
+++ src/components/BoardList/BoardList.jsx
@@ -5,12 +5,14 @@ import Board from "../Board/Board";
 export default function BoardList() {
   const [boards, setBoards] = useState([]);
 
+  // local storage - start
   useEffect(() => {
     const storedBoards = localStorage.getItem("boardData");
     if (storedBoards) {
       setBoards(JSON.parse(storedBoards));
     }
-  }, []);
+  }, [boards]);
+  // local storage - end
   return (
     <>
       <h2>My boards</h2>
@@ -20,12 +22,7 @@ export default function BoardList() {
         {boards &&
           boards.length > 0 &&
           boards.map((board, index) => (
-            <Board
-              key={index}
-              title={board.title}
-              icon={board.icon}
-              bg={board.bg}
-            />
+            <Board key={index} title={board.title} icon={board.icon} />
           ))}
       </ul>
     </>

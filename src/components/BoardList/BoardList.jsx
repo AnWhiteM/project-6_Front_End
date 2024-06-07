d68f491b80ccd8ba4dadd5bb1e633dc221257fe1
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2024-06-07 14:45:47 +0300

    restoringTet1

--- src/components/BoardList/BoardList.jsx
+++ src/components/BoardList/BoardList.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from "react";
 import CreateBoardBtn from "../CreateBoardBtn/CreateBoardBtn";
+import Board from "../Board/Board";
 
-export default function CreateTask() {
+export default function BoardList() {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    const storedBoards = localStorage.getItem("boardData");
+    if (storedBoards) {
+      setBoards(JSON.parse(storedBoards));
+    }
+  }, []);
   return (
     <>
       <h2>My boards</h2>
       <CreateBoardBtn />
       {/* {allBoards.length !== 0 && <ButtonList />} */}
+      <ul>
+        {boards &&
+          boards.length > 0 &&
+          boards.map((board, index) => (
+            <Board
+              key={index}
+              title={board.title}
+              icon={board.icon}
+              bg={board.bg}
+            />
+          ))}
+      </ul>
     </>
   );
 }

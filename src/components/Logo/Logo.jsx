c3cc8b86239443eba9518259b835a4d326a3c05e
TetiZ <tommytaba99@gmail.com>
2024-06-08 02:36:47 +0200

    all sidebar components done
    css

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 
 import svg from "../../img/icons.svg";
+import css from "./Logo.module.css";
 
 export default function Logo() {
   return (
-    <div>
-      {/* <Link to="/home">
-        <svg>
-          <use href={svg + "#icon-logo"}></use>
-        </svg>
-      </Link>
-      <h2>Task Pro</h2> */}
+    <div className={css.logo}>
+      {/* <Link to="/home"> */}
+      <svg className={css.icon} width="32" height="32">
+        <use href={svg + "#icon-logo"}></use>
+      </svg>
+      {/* </Link> */}
+      <p>Task Pro</p>
     </div>
   );
 }

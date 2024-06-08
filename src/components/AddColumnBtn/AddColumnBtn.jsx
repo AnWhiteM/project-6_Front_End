e944688865c01510c341cdb3e9c4357069739d45
Esnobart <offrichterrr@gmail.com>
2024-06-08 16:17:45 +0100

    first modal

--- src/components/AddColumnBtn/AddColumnBtn.jsx
+++ src/components/AddColumnBtn/AddColumnBtn.jsx
@@ -1,9 +1,22 @@
+import { CreateColumn } from "../CreateColumn/CreateColumn";
 import css from "./AddColumnBtn.module.css";
+import { useState } from "react";
 
 export const AddColumnBtn = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openColumnCreateModal() {
+    setIsOpen(true)
+  }
+
+  function closeColumnCreateModal() {
+    setIsOpen(false)
+  }
+
   return (
     <div>
-      <button className={css.btn}>Add another column</button>
+      <button className={css.btn} onClick={openColumnCreateModal}>Add another column</button>
+      <CreateColumn isOpen={isOpen} isClose={closeColumnCreateModal} />
     </div>
   );
 };
---
+++ src/components/CreateColumn/CreateColumn.jsx
@@ -0,0 +1,48 @@
+import Modal from "react-modal";
+import css from "./CreateColumn.module.css";
+import * as Yup from 'yup';
+import { Form, Formik, Field } from "formik";
+
+Modal.setAppElement('#root');
+
+export const CreateColumn = (isOpen, isClose) => {
+    const columnModalValidation = Yup.object.shape({
+        columnname: Yup.string().min(3, 'Too short!').max(20, 'Too long!').required('Required!')
+    });
+    const createColumnStyle = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          width: '335px',
+          height: '221px'
+        },
+      };
+
+    return (
+    <>
+    <Modal isOpen={isOpen} onRequestClose={isClose} style={createColumnStyle}>
+        <button type="button" className={css.createColumnModalCloseBtn}>Close</button>
+        <div className={css.createColumnModalContainer}>
+            <h1 className={css.createColumnModalText}>Add column</h1>
+            <Formik
+                initialValues={{ columnname: ''}}
+                validationSchema={columnModalValidation}
+                onSubmit={(values, actions) => {
+
+                }}>
+                    <Form autoComplete="off">
+                        <Field type='text' name='columnname' />
+
+                    </Form>
+
+            </Formik>
+        </div>
+
+    </Modal>
+    </>
+)
+}

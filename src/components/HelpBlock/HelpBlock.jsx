e591e3142be862251e0da04681a52e0ca6e99337
TetiZ <tommytaba99@gmail.com>
2024-06-09 17:48:50 +0200

    cactus -.-

--- src/components/HelpBlock/HelpBlock.jsx
+++ src/components/HelpBlock/HelpBlock.jsx
@@ -18,9 +18,9 @@ export default function HelpBlock() {
       <div>
         <picture>
           <source
-            srcSet="
+            srcSet={`
                 ../../../public/img/1x-sidebar-plant.png   54w,
-                ../../../public/img/2x-sidebar-plant.png 108w"
+                ../../../public/img/2x-sidebar-plant.png 108w`}
             sizes="(min-width: 1440px) 54px, (min-width: 1280px) 54px, (min-width: 768px) 54px, (min-width: 320px) 54px"
           />
           <img

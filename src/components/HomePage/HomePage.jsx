7e8e7578e8398e7bec4703b5630991bff88dcfaa
TetiZ <tommytaba99@gmail.com>
2024-06-09 17:36:24 +0200

    homePage update
    sidebar render corrects

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,19 +3,31 @@ import SideBar from "../SideBar/SideBar";
 import Header from "../Header/Header";
 import { ScreensPage } from "../ScreensPage/ScreensPage";
 import { Toaster } from "react-hot-toast";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const HomePage = () => {
   const [sideBarOpen, setSideBarOpen] = useState(true);
+  const [screenSize, setScreenSize] = useState({ width: window.innerWidth });
 
   const toggleSideBar = () => {
     setSideBarOpen(!sideBarOpen);
   };
 
+  const resizeHandler = () => {
+    setScreenSize({ width: window.innerWidth });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
   return (
     <>
       <div className={css.container}>
-        {sideBarOpen && (
+        {(screenSize.width >= 1440 || sideBarOpen) && (
           <div className={css.sideB}>
             <SideBar />
           </div>

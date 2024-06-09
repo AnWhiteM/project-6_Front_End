ec2112a790e8b8e3037210ed41e825c32c65fc5b
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2024-06-09 14:20:30 +0300

    renaming Layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
-import { Layout } from "../Layout/Layout";
+import { HomePage } from "../HomePage/HomePage";
 import { WelcomeComponent } from "../WelcomeComponent/WelcomeComponent";
 
 export const App = () => {
   const isLoggedIn = true;
-  return <>{isLoggedIn ? <Layout /> : <WelcomeComponent />}</>;
+  return <>{isLoggedIn ? <HomePage /> : <WelcomeComponent />}</>;
 };

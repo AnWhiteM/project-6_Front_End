9610c74d66565fc2ccfad2849ce35b0ffa3c21b2
Nazar-Kotiv <kotnazar20@gmail.com>
2024-06-11 11:11:08 +0200

    fix-commit

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,47 +1,117 @@
+// import { Formik, Form, Field } from "formik";
+// import { logIn } from "../../redux/auth/operations";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import css from "./LoginForm.module.css";
+// import { useEffect, useState } from "react";
+// import toast from "react-hot-toast";
+// import { selectError } from "../../redux/auth/selectror";
+
+// export default function LoginForm() {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const error = useSelector(selectError);
+//   const [submittedWithError, setSubmittedWithError] = useState(false);
+
+//   // const handleSubmit = async (values, actions) => {
+//   //   try {
+//   //     await dispatch(logIn(values));
+//   //     navigate("/home")
+//   //   } catch (error) {
+//   //     console.error("Login failed:", error);
+//   //   }
+//   //   actions.resetForm();
+//   // };
+
+//   const handleSubmit = (values, actions) => {
+//     dispatch(logIn(values));
+//     navigate("/home");
+//     actions.resetForm();
+//   };
+
+//   useEffect(() => {
+//     if (error && submittedWithError) {
+//       toast.error(`Ops, somthing wrong, Try Again!`);
+//     }
+//   }, [error, submittedWithError]);
+
+//   const handleFormSubmit = (values, actions) => {
+//     setSubmittedWithError(true);
+//     handleSubmit(values, actions);
+//   };
+
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const togglePasswordVisibility = (event) => {
+//     event.preventDefault();
+//     setShowPassword(!showPassword);
+//   };
+
+//   return (
+//     <div>
+//       <Formik
+//         initialValues={{
+//           email: "",
+//           password: "",
+//         }}
+//         onSubmit={handleFormSubmit}
+//       >
+//             <Form className={css.form}>
+//               <label htmlFor="email"/>
+//                 <Field type="email" name="email" placeholder ="Enter your email" className={css.input}/>
+//                 <label htmlFor="password"/>
+//                   <div>
+//                     <Field
+//                       type={showPassword ? "text" : "password"}
+//                       name="password"
+//                       className={css.input}
+//                       placeholder="Confirm a password"
+//                     />
+//                     <button type="button "className={css.eye} onClick={togglePasswordVisibility}>
+//                       {/* <svg width="18" height="18" stroke="currentColor">
+//                         <use
+//                           href={`${svg}${showPassword ? "#eye-slash-icon" : "#eye-icon"}`}
+//                         ></use>
+//                       </svg> */}
+//                     </button>
+//                 </div>
+
+//                 <button type="submit" className={css.button}>Log in Now</button>
+//           </Form>
+//         </Formik>
+//     </div>
+//   );
+// }
 
 import { Formik, Form, Field } from "formik";
 import { logIn } from "../../redux/auth/operations";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import css from "./LoginForm.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import toast from "react-hot-toast";
-import { selectError } from "../../redux/auth/selectror";
-
-
+// import { selectError } from "../../redux/auth/selectror";
+import { unwrapResult } from "@reduxjs/toolkit";
 
 export default function LoginForm() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const error = useSelector(selectError);
+  // const error = useSelector(selectError);
   const [submittedWithError, setSubmittedWithError] = useState(false);
 
-  // const handleSubmit = async (values, actions) => {
-  //   try {
-  //     await dispatch(logIn(values));
-  //     navigate("/home")
-  //   } catch (error) {
-  //     console.error("Login failed:", error);
-  //   }
-  //   actions.resetForm();
-  // };
-
-  const handleSubmit = (values, actions) => {
-    dispatch(logIn(values));
-    navigate("/home");
-    actions.resetForm();
-  };
-
-  useEffect(() => {
-    if (error && submittedWithError) {
-      toast.error(`Ops, somthing wrong, Try Again!`);
-    }
-  }, [error, submittedWithError]);
-
-  const handleFormSubmit = (values, actions) => {
+  const handleSubmit = async (values, actions) => {
     setSubmittedWithError(true);
-    handleSubmit(values, actions);
+    try {
+      const resultAction = await dispatch(logIn(values));
+      unwrapResult(resultAction);
+      navigate("/home");
+    } catch (error) {
+      toast.error(`Ops, something went wrong, Try Again!`);
+      console.error("Login failed:", error);
+    }
+    actions.resetForm();
   };
 
   const [showPassword, setShowPassword] = useState(false);
@@ -50,7 +120,6 @@ export default function LoginForm() {
     event.preventDefault();
     setShowPassword(!showPassword);
   };
- 
 
   return (
     <div>
@@ -59,31 +128,42 @@ export default function LoginForm() {
           email: "",
           password: "",
         }}
-        onSubmit={handleFormSubmit}
+        onSubmit={handleSubmit}
       >
-            <Form className={css.form}>
-              <label htmlFor="email"/>
-                <Field type="email" name="email" placeholder ="Enter your email" className={css.input}/>
-                <label htmlFor="password"/>
-                  <div>
-                    <Field
-                      type={showPassword ? "text" : "password"}
-                      name="password"
-                      className={css.input}
-                      placeholder="Confirm a password"
-                    />
-                    <button type="button "className={css.eye} onClick={togglePasswordVisibility}>
-                      {/* <svg width="18" height="18" stroke="currentColor">
-                        <use
-                          href={`${svg}${showPassword ? "#eye-slash-icon" : "#eye-icon"}`}
-                        ></use>
-                      </svg> */}
-                    </button>
-                </div>
+        <Form className={css.form}>
+          <label htmlFor="email" />
+          <Field
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            className={css.input}
+          />
+          <label htmlFor="password" />
+          <div>
+            <Field
+              type={showPassword ? "text" : "password"}
+              name="password"
+              className={css.input}
+              placeholder="Enter your password"
+            />
+            <button
+              type="button"
+              className={css.eye}
+              onClick={togglePasswordVisibility}
+            >
+              {/* <svg width="18" height="18" stroke="currentColor">
+                <use
+                  href={`${svg}${showPassword ? "#eye-slash-icon" : "#eye-icon"}`}
+                ></use>
+              </svg> */}
+            </button>
+          </div>
 
-                <button type="submit" className={css.button}>Log in Now</button>
-          </Form>
-        </Formik>
+          <button type="submit" className={css.button}>
+            Log in Now
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 }
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,19 +1,19 @@
-import { Formik, Form, Field} from 'formik';
-import { register } from '../../redux/auth/operations';
-import { useDispatch } from 'react-redux';
-import css from "./RegistrationForm.module.css"
+import { Formik, Form, Field } from "formik";
+import { register } from "../../redux/auth/operations";
+import { useDispatch } from "react-redux";
+import css from "./RegistrationForm.module.css";
 import svg from "../../img/icons.svg";
-import { useState } from 'react';
-
-
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const RegistrationForm = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = (values, actions) => {
     console.log(values);
     dispatch(register(values));
+    navigate("/auth/login");
     actions.resetForm();
   };
 
@@ -24,44 +24,57 @@ export const RegistrationForm = () => {
     setShowPassword(!showPassword);
   };
 
-
   return (
     <div>
-      
       <Formik
-      
-      initialValues={{ 
-        name: '', 
-        email: '', 
-        password: '', 
-      }}
-      onSubmit={handleSubmit}
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+        }}
+        onSubmit={handleSubmit}
       >
-            <Form className={css.form}>
-              <label htmlFor="name"/>
-              <Field type="name" name="name" placeholder ="Enter your name" className={css.input}/>
-              <label htmlFor="email"/>
-                <Field type="email" name="email" placeholder ="Enter your email" className={css.input}/>
-                <label htmlFor="password"/>
-                  <div>
-                    <Field
-                      type={showPassword ? "text" : "password"}
-                      name="password"
-                      className={css.input}
-                      placeholder="Create a password"
-                    />
-                    <button type="button "className={css.eye} onClick={togglePasswordVisibility}>
-                      {/* <svg width="18" height="18" stroke="currentColor">
+        <Form className={css.form}>
+          <label htmlFor="name" />
+          <Field
+            type="name"
+            name="name"
+            placeholder="Enter your name"
+            className={css.input}
+          />
+          <label htmlFor="email" />
+          <Field
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            className={css.input}
+          />
+          <label htmlFor="password" />
+          <div>
+            <Field
+              type={showPassword ? "text" : "password"}
+              name="password"
+              className={css.input}
+              placeholder="Create a password"
+            />
+            <button
+              type="button "
+              className={css.eye}
+              onClick={togglePasswordVisibility}
+            >
+              {/* <svg width="18" height="18" stroke="currentColor">
                         <use
                           href={`${svg}${showPassword ? "#eye-slash-icon" : "#eye-icon"}`}
                         ></use>
                       </svg> */}
-                    </button>
-                </div>
+            </button>
+          </div>
 
-                <button type="submit" className={css.button}>Register Now</button>
-          </Form>
-        </Formik>
+          <button type="submit" className={css.button}>
+            Register Now
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };

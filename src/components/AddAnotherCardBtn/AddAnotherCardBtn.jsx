30b5cf413febbf21e3362e449e68697b0eef558d
Esnobart <122377780+Esnobart@users.noreply.github.com>
2024-06-11 16:32:09 +0100

    Update AddAnotherCardBtn.jsx

--- src/components/AddAnotherCardBtn/AddAnotherCardBtn.jsx
+++ src/components/AddAnotherCardBtn/AddAnotherCardBtn.jsx
@@ -1,10 +1,22 @@
 import css from "./AddAnotherCardBtn.module.css";
 import svg from "../../img/icons.svg";
+import { useState } from "react";
+import { CreateCard } from "../CreateCard/CreateCard.jsx"
 
 export const AddAnotherCardBtn = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    
+    const openModal = () => {
+        setIsOpen = true
+    }
+        
+    const closeModal = () => {
+        setIsOpen = false
+    }
+    
   return (
     <div className={css.container}>
-      <button className={css.btn}>
+      <button className={css.btn} onClick={openModal}
         <div className={css.iconBox}>
           <svg className={css.icon} width="14px" height="14px">
             <use href={svg + "#icon-plus"}></use>
@@ -12,6 +24,8 @@ export const AddAnotherCardBtn = () => {
         </div>
         Add another card
       </button>
+    {isOpen &&
+        <CreateCard isOpen={isOpen} isClose={closeModal} />
     </div>
   );
 };

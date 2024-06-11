435971e9df3aa211cd77c427411bf389695946bc
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2024-06-11 11:21:23 +0300

    auth styles

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,6 +3,8 @@ import { logIn } from '../../redux/auth/operations';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import css from "./LoginForm.module.css"
+import svg from "../../img/icons.svg";
+import { useState } from 'react';
 
 export default function LoginForm() {
   const dispatch = useDispatch();
@@ -16,6 +18,14 @@ export default function LoginForm() {
     }
     actions.resetForm();
   };
+
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = (event) => {
+    event.preventDefault();
+    setShowPassword(!showPassword);
+  };
  
   return (
     <div>
@@ -27,13 +37,26 @@ export default function LoginForm() {
         onSubmit={handleSubmit}
       >
             <Form className={css.form}>
-                <label>Enter your Email</label>
-                <Field type="email" name="email"/>
+              <label htmlFor="email"/>
+                <Field type="email" name="email" placeholder ="Enter your email" className={css.input}/>
+                <label htmlFor="password"/>
+                  <div>
+                    <Field
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      className={css.input}
+                      placeholder="Confirm a password"
+                    />
+                    <button type="button "className={css.eye} onClick={togglePasswordVisibility}>
+                      {/* <svg width="18" height="18" stroke="currentColor">
+                        <use
+                          href={`${svg}${showPassword ? "#eye-slash-icon" : "#eye-icon"}`}
+                        ></use>
+                      </svg> */}
+                    </button>
+                </div>
 
-                <label>Confirm a password</label>
-                <Field type="password" name="password"/>
-
-                <button type="submit">Log In Now</button>
+                <button type="submit" className={css.button}>Log in Now</button>
           </Form>
         </Formik>
     </div>
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -2,6 +2,10 @@ import { Formik, Form, Field} from 'formik';
 import { register } from '../../redux/auth/operations';
 import { useDispatch } from 'react-redux';
 import css from "./RegistrationForm.module.css"
+import svg from "../../img/icons.svg";
+import { useState } from 'react';
+
+
 
 
 export const RegistrationForm = () => {
@@ -11,6 +15,13 @@ export const RegistrationForm = () => {
     console.log(values);
     dispatch(register(values));
     actions.resetForm();
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = (event) => {
+    event.preventDefault();
+    setShowPassword(!showPassword);
   };
 
 
@@ -27,16 +38,28 @@ export const RegistrationForm = () => {
       onSubmit={handleSubmit}
       >
             <Form className={css.form}>
-              <label>Enter your name</label>
-              <Field type="name" name="name"/>
+              <label htmlFor="name"/>
+              <Field type="name" name="name" placeholder ="Enter your name" className={css.input}/>
+              <label htmlFor="email"/>
+                <Field type="email" name="email" placeholder ="Enter your email" className={css.input}/>
+                <label htmlFor="password"/>
+                  <div>
+                    <Field
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      className={css.input}
+                      placeholder="Create a password"
+                    />
+                    <button type="button "className={css.eye} onClick={togglePasswordVisibility}>
+                      {/* <svg width="18" height="18" stroke="currentColor">
+                        <use
+                          href={`${svg}${showPassword ? "#eye-slash-icon" : "#eye-icon"}`}
+                        ></use>
+                      </svg> */}
+                    </button>
+                </div>
 
-                <label>Enter your Email</label>
-                <Field type="email" name="email"/>
-
-                <label>Create a password</label>
-                <Field type="password" name="password"/>
-
-                <button type="submit">Register Now</button>
+                <button type="submit" className={css.button}>Register Now</button>
           </Form>
         </Formik>
     </div>

f18958110455b18f8f644ca6dcfcda5094165a05
Dadayat <136356333+Dadayat@users.noreply.github.com>
2024-06-17 00:13:07 +0300

    change

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -1,30 +1,3 @@
-// import { useSelector } from "react-redux";
-// import { AddColumnBtn } from "../AddColumnBtn/AddColumnBtn";
-// import { TaskColumn } from "../TaskColumn/TaskColumn";
-// import css from "./MainDashboard.module.css";
-
-// import {
-//   selectLoading,
-//   selectError,
-//   selectCurrentBoard,
-// } from "../../redux/boards/selectors";
-
-// export const MainDashboard = () => {
-//   const loading = useSelector(selectLoading);
-//   const error = useSelector(selectError);
-//   const currentBoard = useSelector(selectCurrentBoard);
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>Error loading boards</p>;
-
-//   return (
-//     <div className={css.container}>
-//       <TaskColumn board={currentBoard} />
-//       <AddColumnBtn board={currentBoard} />
-//     </div>
-//   );
-// };
-
 import { useSelector } from "react-redux";
 import { AddColumnBtn } from "../AddColumnBtn/AddColumnBtn";
 import { TaskColumn } from "../TaskColumn/TaskColumn";
@@ -51,7 +24,6 @@ export const MainDashboard = () => {
         backgroundPosition: "center",
       }
     : { backgroundColor: "#1F1F1F" };
-
   return (
     <div className={css.container} style={backgroundStyle}>
       <TaskColumn board={currentBoard} />

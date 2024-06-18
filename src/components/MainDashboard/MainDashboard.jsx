44d8c3630a58d1d93f44fea7e0ae3e56b5df27ac
TetiZ <tommytaba99@gmail.com>
2024-06-18 09:31:53 +0200

    horizontal scroll added

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -56,7 +56,6 @@ export const MainDashboard = () => {
     >
       <div className={css.wrapper}>
         <TaskColumn board={currentBoard} />
-        <AddColumnBtn board={currentBoard} />
       </div>
     </div>
   );

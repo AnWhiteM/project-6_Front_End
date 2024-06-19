ed6acb33b4885dc5ed0f51095eda6aa03857ade5
Esnobart <offrichterrr@gmail.com>
2024-06-19 16:13:44 +0100

    add React-datepicker

--- src/components/CreateCardModal/CreateCardModal.jsx
+++ src/components/CreateCardModal/CreateCardModal.jsx
@@ -5,94 +5,15 @@ import { Form, Formik, Field, ErrorMessage } from "formik";
 import svg from "../../img/icons.svg";
 import { useDispatch } from "react-redux";
 import { addTask } from "../../redux/tasks/operations";
-import { DatePicker } from "@mui/x-date-pickers";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
-import dayjs from "dayjs";
-import Stack from '@mui/material/Stack';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeft from '@mui/icons-material/ChevronLeft';
-import ChevronRight from '@mui/icons-material/ChevronRight';
-import { styled } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import SvgIcon from '@mui/material/SvgIcon';
 
 Modal.setAppElement("#root");
 
 export const CreateCard = ({ isOpen, isClose, column }) => {
-  const [selectedDate, setSelectedDate] = useState(dayjs());
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const dispatch = useDispatch();
-
-  const changeDate = (valueDate) => {
-    setSelectedDate(valueDate)
-  }
-
-  const CustomCalendarHeaderRoot = styled('div')({
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottom: '1px solid #000000',
-    backgroundColor: '#1F1F1F'
-  });
-
-  const CustomDatePicker = styled(DatePicker)({
-    '& .MuiInputBase-root': {
-      display: 'flex',
-      alignItems: 'center',
-      backgroundColor: '#151515',
-      color: '#BEDBB0',
-      fontFamily: 'Poppins, sans-serif !important',
-      fontSize: '14px',
-      fontWeight: '500',
-      lineHeight: '21px',
-      letterSpacing: '-0.02em',
-      textAlign: 'left',
-    },
-    '& .MuiInputBase-input': {
-      padding: '0',
-      width: 'auto',
-    },
-    '& .MuiOutlinedInput-notchedOutline': {
-      border: 'none',
-    },
-    '&:hover .MuiOutlinedInput-notchedOutline': {
-      borderColor: '#FFFFFF',
-    }
-  });
-  
-
-  const calendarIcon = () => {
-    return (
-      <SvgIcon>
-        <use className={css.chevronIconDown} href={svg + "#chevron-down-icon"} />
-      </SvgIcon>
-    );
-  }
-
-  function CustomCalendarHeader(props) {
-    const { currentMonth, onMonthChange } = props;
-  
-    const selectNextMonth = () => onMonthChange(currentMonth.add(1, 'month'), 'left');
-    const selectPreviousMonth = () =>
-      onMonthChange(currentMonth.subtract(1, 'month'), 'right');
-  
-    return (
-      <CustomCalendarHeaderRoot>
-        <Stack spacing={1} direction="row">
-          <IconButton onClick={selectPreviousMonth} title="Previous month">
-            <ChevronLeft className={css.chevron} />
-          </IconButton>
-        </Stack>
-        <Typography variant="body2" className={css.customTypography}>{currentMonth.format('MMMM YYYY')}</Typography>
-        <Stack spacing={1} direction="row">
-          <IconButton onClick={selectNextMonth} title="Next month">
-            <ChevronRight className={css.chevron} />
-          </IconButton>
-        </Stack>
-      </CustomCalendarHeaderRoot>
-    );
-  }
 
   const radioBtns = [
     {
@@ -221,15 +142,12 @@ export const CreateCard = ({ isOpen, isClose, column }) => {
               </label>
               <div className={css.createCardModalDateContainer}>
                 <p className={css.deadlineText}>Deadline</p>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <CustomDatePicker
-                  value={selectedDate}
-                  onChange={changeDate}
-                  format={dayjs(selectedDate).format('DD/MM/YYYY') === dayjs().format('DD/MM/YYYY') ? 'Today, MMMM DD' : 'dddd, MMMM DD'}
-                  slots={{ openPickerIcon: calendarIcon, calendarHeader: CustomCalendarHeader }}
-                  minDate={dayjs()}
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  dateFormat="EEEE, MMMM dd"
+                  minDate={new Date()}
                 />
-              </LocalizationProvider>
               </div>
               <button type="submit" className={css.createCardModalSubmit}>
                 <span className={css.createCardModalSpan}>
--- src/components/EditCardModal/EditCardModal.jsx
+++ src/components/EditCardModal/EditCardModal.jsx
@@ -7,26 +7,14 @@ import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { updateTask } from "../../redux/tasks/operations.js";
 import { useParams } from "react-router-dom";
-import { DatePicker } from "@mui/x-date-pickers";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
-import dayjs from "dayjs";
-import Stack from '@mui/material/Stack';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeft from '@mui/icons-material/ChevronLeft';
-import ChevronRight from '@mui/icons-material/ChevronRight';
-import { styled } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import SvgIcon from '@mui/material/SvgIcon';
 
 Modal.setAppElement("#root");
 
 export const EditCard = ({ isOpen, isClose, task }) => {
-  const [selectedDate, setSelectedDate] = useState(dayjs(task.deadline));
-  const changeDate = (valueDate) => {
-    setSelectedDate(valueDate)
-  }
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   const dispatch = useDispatch();
   const { deskId } = useParams();
@@ -42,72 +30,6 @@ export const EditCard = ({ isOpen, isClose, task }) => {
   });
 
   const taskEditNotify = () => toast.success("You edited the task");
-
-  const CustomCalendarHeaderRoot = styled('div')({
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottom: '1px solid #000000',
-    backgroundColor: '#1F1F1F'
-  });
-
-  const CustomDatePicker = styled(DatePicker)({
-    '& .MuiInputBase-root': {
-      display: 'flex',
-      alignItems: 'center',
-      backgroundColor: '#151515',
-      color: '#BEDBB0',
-      fontFamily: 'Poppins, sans-serif !important',
-      fontSize: '14px',
-      fontWeight: '500',
-      lineHeight: '21px',
-      letterSpacing: '-0.02em',
-      textAlign: 'left',
-    },
-    '& .MuiInputBase-input': {
-      padding: '0',
-      width: 'auto',
-    },
-    '& .MuiOutlinedInput-notchedOutline': {
-      border: 'none',
-    },
-    '&:hover .MuiOutlinedInput-notchedOutline': {
-      borderColor: '#FFFFFF',
-    }
-  });
-  
-
-  const calendarIcon = () => {
-    return (
-      <SvgIcon>
-        <use className={css.chevronIconDown} href={svg + "#chevron-down-icon"} />
-      </SvgIcon>
-    );
-  }
-
-  function CustomCalendarHeader(props) {
-    const { currentMonth, onMonthChange } = props;
-  
-    const selectNextMonth = () => onMonthChange(currentMonth.add(1, 'month'), 'left');
-    const selectPreviousMonth = () =>
-      onMonthChange(currentMonth.subtract(1, 'month'), 'right');
-  
-    return (
-      <CustomCalendarHeaderRoot>
-        <Stack spacing={1} direction="row">
-          <IconButton onClick={selectPreviousMonth} title="Previous month">
-            <ChevronLeft className={css.chevron} />
-          </IconButton>
-        </Stack>
-        <Typography variant="body2" className={css.customTypography}>{currentMonth.format('MMMM YYYY')}</Typography>
-        <Stack spacing={1} direction="row">
-          <IconButton onClick={selectNextMonth} title="Next month">
-            <ChevronRight className={css.chevron} />
-          </IconButton>
-        </Stack>
-      </CustomCalendarHeaderRoot>
-    );
-  }
 
   const handleSubmit = (values) => {
     const updatedTask = {
@@ -223,15 +145,12 @@ export const EditCard = ({ isOpen, isClose, task }) => {
 
               <div className={css.createCardModalDateContainer}>
                 <p className={css.deadlineText}>Deadline</p>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <CustomDatePicker
-                  value={selectedDate}
-                  onChange={changeDate}
-                  format={dayjs(selectedDate).format('DD/MM/YYYY') === dayjs().format('DD/MM/YYYY') ? 'Today, MMMM DD' : 'dddd, MMMM DD'}
-                  slots={{ openPickerIcon: calendarIcon, calendarHeader: CustomCalendarHeader }}
-                  minDate={dayjs()}
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  dateFormat="EEEE, MMMM dd"
+                  minDate={new Date()}
                 />
-              </LocalizationProvider>
               </div>
 
               <button type="submit" className={css.editCardModalSubmit}>

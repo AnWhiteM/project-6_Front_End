5c8f5cb1e8e1cbb18a2a90b55b376fe9dc637b97
VitaliiShc <vitaliy.shchukin@gmail.com>
2024-06-20 10:38:38 +0300

    styles for need-help form done

--- src/components/ModalHelp/ModalHelp.jsx
+++ src/components/ModalHelp/ModalHelp.jsx
@@ -4,20 +4,18 @@ import css from "./ModalHelp.module.css";
 import * as Yup from "yup";
 import toast from "react-hot-toast";
 
+import { selectUser } from "../../redux/auth/selectors";
+import { useSelector } from "react-redux";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useDispatch } from "react-redux";
 import { sendHelpMessage } from "../../redux/boards/operations";
 
 export default function ModalHelp({ isOpen, closeModal }) {
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   const ValidEditionSchema = Yup.object().shape({
-    email: Yup.string()
-      .email("Must be a valid email!")
-      .matches(/\.com$|\.ua$/, "Email must end with .com or .ua")
-      .min(1, "Too Short Email!")
-      .max(35, "Too Long Email!")
-      .required("Required"),
+    email: Yup.string().email("Must be a valid email!").required("Required"),
     comment: Yup.string()
       .min(4, "Too Short Comment!")
       .max(300, "Too Long Comment!")
@@ -25,7 +23,7 @@ export default function ModalHelp({ isOpen, closeModal }) {
   });
   const Notify = () =>
     toast.success(
-      "Thanks for contacting us! We've received your message and will get back to you soon."
+      "Thanks for contacting us! We've received your message and will get back to you soon"
     );
 
   const handleSubmit = (values, actions) => {
@@ -33,7 +31,6 @@ export default function ModalHelp({ isOpen, closeModal }) {
       email: values.email,
       comment: values.comment,
     };
-    console.log(newMessage);
     actions.resetForm();
     dispatch(sendHelpMessage(newMessage));
     Notify();
@@ -49,48 +46,58 @@ export default function ModalHelp({ isOpen, closeModal }) {
     >
       <h2 className={css.title}>Need help</h2>
       <svg className={css.icon} onClick={closeModal} width="18px" height="18px">
-        <use className={css.icon} href={svg + "#x-close"}></use>
+        <use href={svg + "#x-close"}></use>
       </svg>
       <Formik
-        initialValues={{ email: "", comment: "" }}
+        initialValues={{ email: user.email, comment: "" }}
         onSubmit={handleSubmit}
         validationSchema={ValidEditionSchema}
       >
-        <Form autoComplete="off">
-          <div className={css.formGroup}>
-            <Field
-              className={css.input}
-              type="email"
-              name="email"
-              placeholder="Email address "
-            />
-            <ErrorMessage
-              name="email"
-              component="div"
-              className={css.errorMessage}
-            />
-          </div>
+        {({ touched, errors }) => (
+          <Form autoComplete="off">
+            <div className={css.formGroup}>
+              <div className={css.errorContainer}>
+                <ErrorMessage
+                  name="email"
+                  component="span"
+                  className={css.error}
+                />
+              </div>
+              <Field
+                type="email"
+                name="email"
+                placeholder="Email address"
+                className={`${css.input} ${
+                  errors.email && touched.email ? css.inputError : ""
+                }`}
+              />
+            </div>
 
-          <div className={css.formGroup}>
-            <Field
-              className={css.inputText}
-              type="text"
-              name="comment"
-              placeholder="Comment"
-              rows="5"
-              component="textarea"
-            />
-            <ErrorMessage
-              name="comment"
-              component="div"
-              className={css.errorMessage2}
-            />
-          </div>
+            <div className={css.formGroup}>
+              <div className={css.errorContainer}>
+                <ErrorMessage
+                  name="comment"
+                  component="span"
+                  className={css.error}
+                />
+              </div>
+              <Field
+                className={`${css.textArea} ${
+                  errors.comment && touched.comment ? css.textAreaError : ""
+                }`}
+                type="text"
+                name="comment"
+                placeholder="Comment"
+                rows="5"
+                component="textarea"
+              />
+            </div>
 
-          <button className={css.button} type="submit">
-            Send
-          </button>
-        </Form>
+            <button className={css.button} type="submit">
+              Send
+            </button>
+          </Form>
+        )}
       </Formik>
     </Modal>
   );
